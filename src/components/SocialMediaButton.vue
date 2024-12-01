e481640928a059c1ccf6f34e1992005ff758599b
琦琦 <2413915435@qq.com>
2024-12-01 20:44:23 +0800

    button init

--- src/components/SocialMediaButton.vue
+++ src/components/SocialMediaButton.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
+import {config} from "../assets/config";
 
+defineProps<{
+  title: string,
+  url: string
+}>();
+
+const buttoncolor = config.theme.buttonColor
 </script>
 
 <template>
-
+  <div class="container bg-red-900">
+    <a :href="url" target="_blank">{{ title }}</a>
+  </div>
 </template>
 
 <style scoped>
-
+  div{
+    display: block;
+  }
 </style>
