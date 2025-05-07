e16e21e8a8e57c157614dd4c0f888abc6afe6c2c
Meow <meow@gemdzqq.com>
2025-05-07 17:05:44 +0800

    🎉feat: enriching contents

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -30,7 +30,7 @@ export const config: Config = {
         {
             id: 2,
             title: "Resume",
-            url: 'https://meow.gemdzqq.com',
+            url: 'https://me.gemdzqq.com',
         },
         {
             id: 3,
